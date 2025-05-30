807fc918c4b5042d624e395fd16e14e7df2b3ee3
gitHaq2531 <afzaulhaque1@gmail.com>
2025-05-30 15:37:41 +0530

    m3

--- src/a_new_programs/NewPClas.java
+++ src/a_new_programs/NewPClas.java
@@ -7,6 +7,12 @@ public static void main(String[] args)
 	System.out.println("hii");
 	System.out.println("hii");
 	System.out.println("hii");
+	System.out.println("hii");
+	System.out.println("hii");
+	System.out.println("hii");
+	System.out.println("hii");
+	System.out.println("hii");
+	System.out.println("hii");
 
 }
 }
